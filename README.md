e11a30c3ea8514ac400cebcef1ebaf4429da01b7
rogtavares <rogerio@almeidatavares.com.br>
2023-11-18 23:23:09 -0300

    commint v5  Update
    DiasEntreDatas

--- README.md
+++ README.md
@@ -3,5 +3,8 @@ no mercado de desenvolvimento!
 Testando um pouco o GitHub Copilot utilizando Java como linguagem,
 vamos ver um pouquinho na prática do que ela é capaz...
 
+dizem q GitHub Copilot pode ajudá-lo a trabalhar, oferecendo 
+sugestões embutidas enquanto você codifica.
+
 
 #java #copilot #inovação #AI #pair programmer
--- src/application/Hello.java
+++ src/application/Hello.java
@@ -1,2 +1,21 @@
-package application;public class Hello {
+public class DiasEntreDatas {
+
+    public static void main(String[] args) {
+
+        // Define as datas
+        Date data1 = new Date(2023, 11, 18);
+        Date data2 = new Date(2023, 12, 25);
+
+        // Calcula a diferença entre as datas
+        long diferenca = data2.getTime() - data1.getTime();
+
+        // Converte a diferença para dias
+        long dias = diferenca / (1000 * 60 * 60 * 24);
+
+        // Imprime o número de dias
+        System.out.println("O número de dias entre as datas é: " + dias);
+    }
+}
+
+
 }
